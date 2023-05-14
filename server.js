853466ee90d26aea9e721745867eb6c9b2cecc2d
Kendall Malik Howard <kendallhoward04@gmail.com>
2023-05-14 11:27:44 -0700

    config for vercel

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT;
 
 app.get("/", (req, res)=> {
     res.send("Hello World!")
 });
 
-app.listen(3000);
+app.listen(PORT, () => {
+    console.log(`Sevrer is running on port ${PORT}`);
+})
+
+module.exports = app
